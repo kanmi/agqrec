0c408c723ed7ed1e1a0843f2008c147aff7d11a8
kanmi <kanmi@kanmi.org>
2016-01-30 00:49:08 +0900

    Add Clockwork#reload!

--- lib/clock.rb
+++ lib/clock.rb
@@ -4,23 +4,30 @@ require_relative 'config'
 require_relative 'schedule'
 
 module Clockwork
-  config = Config.new
-  schedules = Schedule.new(config).all
+  @init_proc = Proc.new do
+    schedules = Schedule.new.all
 
-  ( config.notification_methods +
-    schedules.map { |s| s[:provider] }.uniq ).each do |name|
-    require_relative "../plugin/" + name
+    schedules.map { |s| s[:provider] }.uniq.each do |name|
+      require_relative "../plugin/" + name
+    end
+
+    configure do |conf|
+      conf[:thread] = true
+    end
+    
+    schedules.each do |schedule|
+	  every(schedule[:interval].days, schedule[:title], at: schedule.to_clockwork_at) do
+        sleep(60-(schedule[:margin]%60))
+	    puts "Running #{schedule["title"]}, at #{}"
+        Object.const_get(schedule[:provider]).record(schedule)
+      end
+    end
   end
 
-  configure do |conf|
-    conf[:thread] = true
-  end
-  
-  schedules.each do |schedule|
-	every(schedule[:interval].days, schedule[:title], at: schedule.to_clockwork_at) do
-      sleep(60-(schedule[:margin]%60))
-	  puts "Running #{schedule["title"]}, at #{}"
-      Object.const_get(schedule[:provider]).record(schedule)
+  class << self
+    def reload!
+      Clockwork.clear!
+      @init_proc.call
     end
   end
 end
