e2a578b7fb053370a37809b6b34e9672fea9fd4d
kanmi <kanmi@kanmi.org>
2016-01-29 21:19:15 +0900

    Load margin value from schedule file

--- schedule.rb
+++ schedule.rb
@@ -1,16 +1,19 @@
 require 'json'
+require_relative 'config'
 
 class Schedule
   include Enumerable
   
   def initialize(schedule_file = "./schedule.json")
+    config = Config.new
     schedule = JSON.parse(File.read(schedule_file)) rescue []
     @schedule = schedule.map do |s|
       {
         title: s['title'],
         from: s['from'],
         to: s['to'],
-        sound_only: s['sound_only'] || false
+        sound_only: s['sound_only'] || false,
+        margin: s['margin'] || config.margin
       }
     end
   end
