35eae74489662bc31de4cf9581eb1ba02983ad43
kanmi <kanmi@kanmi.org>
2016-01-31 12:50:51 +0900

    Fix recording command

--- plugin/AGQR.rb
+++ plugin/AGQR.rb
@@ -6,7 +6,7 @@ module AGQR
       ::Config.add_field(name, {})
       ::Config.AGQR[:stream_url] ||= 'rtmp://fms-base1.mitene.ad.jp/agqr/aandg22'
       ::Config.AGQR[:rtmp_cmd]       ||= "rtmpdump -q -r #{Config.AGQR[:stream_url]} --live -o '#{Config.save_path}' -B %s 2>&1 >/dev/null"
-      ::Config.AGQR[:rtmp_cmd_sound] ||= "rtmpdump -q -r #{Config.AGQR[:stream_url]} --live -B %s 2>/dev/null | ffmpeg -i pipe:0 -acodec copy -f #{Config.save_path}"
+      ::Config.AGQR[:rtmp_cmd_sound] ||= "rtmpdump -q -r #{Config.AGQR[:stream_url]} --live -B %s 2>/dev/null | ffmpeg -loglevel quiet -i pipe:0 -acodec copy #{Config.save_path}"
       ::Config.AGQR
     end
 
